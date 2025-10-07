73c578d07dc34a596562ddac92c43111bbff13e1
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-10-07 13:31:59 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,7 +27,7 @@ exports.googleLogin = async (req, res) => {
       });
     }
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '31d' });
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '12h' });
     res.status(200).json({ token });
 
   } catch (err) {
