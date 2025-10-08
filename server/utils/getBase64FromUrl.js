52ed9b06a528bb8a0d526da0949df4c75ede838f
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-10-08 17:18:31 +0530

    Delete server directory

--- server/utils/getBase64FromUrl.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-async function getBase64FromUrl(url) {
-  const response = await axios.get(url, { responseType: 'arraybuffer' });
-  return Buffer.from(response.data, 'binary').toString('base64');
-}
-
-module.exports = getBase64FromUrl;
