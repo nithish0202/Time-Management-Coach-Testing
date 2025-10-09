064e2fc17c924dfb41f2312e32dd84503b603c66
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-10-09 15:55:59 +0530

    Delete Client directory

--- Client/Config.js
+++
@@ -1,7 +0,0 @@
-const BACKEND_URL = "https://time-management-coach-testing.onrender.com";
-
-// const BACKEND_URL = "http://localhost:5000";
-export default BACKEND_URL;
-
-// https://time-management-coach-testing.onrender.com 
-
--- Client/README.md
+++
@@ -1,1 +0,0 @@
-# Time-Management-Coach-frontend
