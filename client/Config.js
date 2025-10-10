3a881f0961cfa444b70fdf31e1c2b7fd959d5bca
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-10-10 10:22:48 +0530

    Update Config.js

--- client/Config.js
+++ client/Config.js
@@ -1,7 +1,2 @@
 const BACKEND_URL = "https://time-management-coach-testing.onrender.com";
 export default BACKEND_URL;
-// const BACKEND_URL = "http://localhost:5000";
-
-
-// https://time-management-coach-testing.onrender.com 
-
